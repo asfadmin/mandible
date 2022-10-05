999fb33a2113d404753a5aedf7c34fa3e53ba89a
Matt Perry <mperry37@alaska.edu>
2022-10-05 12:34:52 -0600

    feat: add h5parser to workflow

---
+++ mandible/h5_parser.py
@@ -0,0 +1,37 @@
+import h5py
+import numpy as np
+
+
+class H5parser(dict):
+    """
+    Parser for *.h5 files
+    Pre: A list of groups to be included,
+    this can be very specific full path or generic groups like 'metadata'
+    Post: Dict with h5 groups as keys
+    """
+    def __init__(self, h5_groups):
+        self.h5_groups = h5_groups
+
+    def _get_h5_dict(self, _, node) -> None:
+        fullname = node.name
+        if isinstance(node, h5py.Dataset):
+            key = fullname.split("/")[-1]
+            if (
+                any([True for group in self.h5_groups if group in fullname])
+            ):
+                node_val = node[()]
+                if isinstance(node_val, np.integer):
+                    node_val = int(node_val)
+                if isinstance(node_val, np.floating):
+                    node_val = float(node_val)
+                if isinstance(node_val, np.ndarray):
+                    node_val = node_val.tolist()
+                    if isinstance(node_val[0], bytes):
+                        node_val = [x.decode("utf-8") for x in node_val]
+                if isinstance(node_val, bytes):
+                    node_val = node_val.decode("utf-8")
+                self[key] = node_val
+
+    def read_file(self, file: str) -> None:
+        with h5py.File(file, "r") as h5f:
+            h5f.visititems(self._get_h5_dict)
