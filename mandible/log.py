7c15bfd7fb973b4a3b8618967b0a0a7d2f758434
Matt Perry <mperry37@alaska.edu>
2022-10-05 14:55:28 -0600

    feat: add log setup to lambda lib

---
+++ mandible/log.py
@@ -0,0 +1,11 @@
+import logging
+import os
+
+
+def init_root_logger():
+    """Set up log levels for lambda using the environment variable"""
+    level = os.getenv("LOG_LEVEL", logging.INFO)
+
+    logging.getLogger().setLevel(level)
+    logging.getLogger("boto3").setLevel(logging.INFO)
+    logging.getLogger("botocore").setLevel(logging.INFO)
