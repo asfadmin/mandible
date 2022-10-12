d826cd9775a1ce77878a60e415e4218b48424030
Matt Perry <mperry37@alaska.edu>
2022-10-12 17:34:18 -0600

    refac: move aws stuff to conftest to reuse

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,25 @@
+import os
 from pathlib import Path
 
+import boto3
 import pytest
+from moto import mock_s3
+
+
+@pytest.fixture
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture
+def s3_resource(aws_credentials):
+    with mock_s3():
+        yield boto3.resource("s3")
 
 
 @pytest.fixture(scope="session")
