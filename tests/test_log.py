19c9b2d5c17ad8a698f54290120bb782138d93d3
Rohan Weeden <reweeden@alaska.edu>
2023-01-13 11:47:35 -0900

    Add context manager for logging full exception chains in lambdas

---
+++ tests/test_log.py
@@ -0,0 +1,35 @@
+import pytest
+from lambdas.lib.log import log_errors
+
+
+def test_log_errors(caplog):
+    with pytest.raises(Exception):
+        with caplog.at_level("ERROR"):
+            with log_errors():
+                raise Exception("test")
+
+    assert "Exception: test" in caplog.text
+
+
+def test_log_errors_configure_types(caplog):
+    with pytest.raises(Exception):
+        with caplog.at_level("ERROR"):
+            # Only log KeyErrors and TypeErrors
+            with log_errors(KeyError, TypeError):
+                raise Exception("test")
+
+    assert "Exception: test" not in caplog.text
+
+
+def test_log_errors_chained(caplog):
+    with pytest.raises(Exception):
+        with caplog.at_level("ERROR"):
+            with log_errors():
+                try:
+                    raise Exception("Cause")
+                except Exception as e:
+                    raise Exception("Result") from e
+
+    assert "Exception: Cause" in caplog.text
+    assert "Exception: Result" in caplog.text
+    assert "The above exception was the direct cause" in caplog.text
