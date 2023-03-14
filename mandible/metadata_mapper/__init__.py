165f7529e859a7621ce78c880e92261c2d387790
Rohan Weeden <reweeden@alaska.edu>
2023-03-14 18:18:48 -0400

    Re-export from correct files

--- mandible/metadata_mapper/__init__.py
+++ mandible/metadata_mapper/__init__.py
@@ -1,9 +1,12 @@
-from .mapper import Context, MetadataMapper, MetadataMapperError
+from .context import Context
+from .format import Format
+from .mapper import MetadataMapper, MetadataMapperError
 from .source import ConfigSourceProvider, PySourceProvider, Source
 
 __all__ = [
     "ConfigSourceProvider",
     "Context",
+    "Format",
     "MetadataMapper",
     "MetadataMapperError",
     "PySourceProvider",
