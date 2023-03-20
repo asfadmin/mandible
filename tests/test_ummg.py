a62a6c2f2991bed217315dd590a8d7d27edb7576
Matt Perry <mperry37@alaska.edu>
2023-03-20 17:19:58 -0600

    feat: add additional test for custom ummg

--- tests/test_ummg.py
+++ tests/test_ummg.py
@@ -1,8 +1,10 @@
 from datetime import datetime, timezone
+from typing import List, Optional
 
 from freezegun import freeze_time
 
 from mandible.umm.ummg import UmmgBase
+from mandible.umm.ummg_types import PgeVersion
 
 
 @freeze_time("2022-08-25 21:45:44.123456")
@@ -39,8 +41,8 @@ def test_ummg():
             ]
         }
     }
-    new_data = UmmgBase(data)
-    res = new_data.get_ummg()
+    ummg_data = UmmgBase(data)
+    res = ummg_data.get_ummg()
     assert res == {
         "CollectionReference": {"EntryTitle": "test-long-name"},
         "DataGranule": {
@@ -82,6 +84,128 @@ def test_ummg():
             {
                 "Type": "GET DATA VIA DIRECT ACCESS",
                 "URL": "s3://test/test.xml"
+            }
+        ],
+        "TemporalExtent": {
+            "RangeDateTime": {
+                "BeginningDateTime": "2022-08-25T21:45:44.123456Z",
+                "EndingDateTime": "2022-08-25T21:45:44.123456Z"
+            }
+        },
+    }
+
+
+class AdditionalAttributeUmmg(UmmgBase):
+    def get_additional_attributes(self) -> List[dict]:
+        return self.product_metadata["additional_attributes"]
+
+    def get_pge_version(self) -> Optional[PgeVersion]:
+        return {"PGEVersion": self.product_metadata["pge_version"]}
+
+
+@freeze_time("2022-08-25 21:45:44.123456")
+def test_custom_ummg():
+    data = {
+        "collection_info": {
+            "short_name": "test-json-short-name",
+            "long_name": "test-json-long-name"
+        },
+        "product_md": {
+            "product_creation_datetime": datetime.now(timezone.utc),
+            "product_start_time": datetime.now(timezone.utc),
+            "product_stop_time": datetime.now(timezone.utc),
+            "mission": "tester-mission-9000",
+            "additional_attributes": [
+                {"test": "test"},
+                {"tester": 1}
+            ],
+            "pge_version": "test-version-1"
+        },
+        "product_files_md": {
+            "product_file":  {
+                "name": "test.json",
+                "uri": "http://test/test.json",
+                "s3uri": "s3://test/test.json",
+                "size": 123456,
+                "key": "test.json",
+                "bucket": "test",
+            },
+            "related_files": [
+                {
+                    "name": "test.json.md5",
+                    "uri": "http://test/test.json.md5",
+                    "s3uri": "s3://test/test.json.md5",
+                    "size": 123,
+                    "key": "test.json.md5",
+                    "bucket": "test",
+                }
+            ]
+        }
+    }
+    ummg_data = AdditionalAttributeUmmg(data)
+    res = ummg_data.get_ummg()
+    assert res == {
+        "AdditionalAttributes": [
+            {
+                "test": "test"
+            },
+            {
+                "tester": 1
+            }
+        ],
+        "CollectionReference": {
+            "EntryTitle": "test-json-long-name"
+        },
+        "DataGranule": {
+            "ArchiveAndDistributionInformation": [
+                {
+                    "Format": "JSON",
+                    "Name": "test.json",
+                    "SizeInBytes": 123456
+                }
+            ],
+            "DayNightFlag": "Unspecified",
+            "Identifiers": [
+                {
+                    "Identifier": "test",
+                    "IdentifierType": "ProducerGranuleId"
+                }
+            ],
+            "ProductionDateTime": "2022-08-25T21:45:44.123456Z"
+        },
+        "GranuleUR": "test-json",
+        "MetadataSpecification": {
+            "Name": "UMM-G",
+            "URL": "https://cdn.earthdata.nasa.gov/umm/granule/v1.6.4",
+            "Version": "1.6.4"
+        },
+        "PGEVersionClass": {
+            "PGEVersion": "test-version-1"
+        },
+        "Platforms": [
+            {
+                "ShortName": "tester-mission-9000"
+            }
+        ],
+        "ProviderDates": [
+            {
+                "Date": "2022-08-25T21:45:44.123456Z",
+                "Type": "Insert"
+            },
+            {
+                "Date": "2022-08-25T21:45:44.123456Z",
+                "Type": "Update"
+            }
+        ],
+        "RelatedUrls": [
+            {
+                "Subtype": "VERTEX",
+                "Type": "GET DATA",
+                "URL": "http://test/test.json"
+            },
+            {
+                "Type": "GET DATA VIA DIRECT ACCESS",
+                "URL": "s3://test/test.json"
             }
         ],
         "TemporalExtent": {
