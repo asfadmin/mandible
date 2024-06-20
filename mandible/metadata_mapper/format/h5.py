8a310d9542ef916e84c0696627d54aa698a6d28a
Rohan Weeden <reweeden@alaska.edu>
2024-06-20 14:05:51 -0400

    Remove unused h5_parser

--- mandible/metadata_mapper/format/h5.py
+++ mandible/metadata_mapper/format/h5.py
@@ -2,8 +2,8 @@ from dataclasses import dataclass
 from typing import IO, Any, ContextManager
 
 import h5py
+import numpy as np
 
-from mandible.h5_parser import normalize
 
 from .format import SimpleFormat
 
@@ -17,3 +17,21 @@ class H5(SimpleFormat):
     @staticmethod
     def _eval_key(data, key: str):
         return normalize(data[key][()])
+
+
+def normalize(node_val: Any) -> Any:
+    if isinstance(node_val, np.bool_):
+        return bool(node_val)
+    if isinstance(node_val, np.integer):
+        return int(node_val)
+    if isinstance(node_val, np.floating):
+        return float(node_val)
+    if isinstance(node_val, np.ndarray):
+        value = node_val.tolist()
+        if isinstance(value[0], bytes):
+            value = [x.decode("utf-8") for x in value]
+        return value
+    if isinstance(node_val, bytes):
+        return node_val.decode("utf-8")
+
+    return node_val
